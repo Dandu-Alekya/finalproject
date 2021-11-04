fd5c9e06f2641b552c894e915e83e71b58a4d79f
Alekya Dandu <alekya.dandu@klh.edu.in>
2021-11-04 15:39:04 +0530

    Added functionality for adding a new todo list item

--- todo list/todolist.js
+++ todo list/todolist.js
@@ -3,3 +3,25 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 
+todoButton.addEventListener('click',addTodo);
+
+function addTodo(event){
+    event.preventDefault();
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    const newtodo = document.createElement("li");
+    newtodo.innerText="hieyaa";
+    newtodo.classList.add("todo-item");
+    todoDiv.appendChild(newtodo);
+    //completed task button
+    const completedtask = document.createElement('button');
+    completedtask.innerHTML='<i class="fas fa-check"></i>';
+    completedtask.classList.add("complete-btn");
+    todoDiv.appendChild(completedtask);
+    //Delete task button
+    const deletetask = document.createElement('button');
+    deletetask.innerHTML='<i class="fas fa-trash"></i>';
+    deletetask.classList.add("complete-btn");
+    todoDiv.appendChild(deletetask);
+    todoList.appendChild(todoDiv);
+}
