01f515464789117a3cbb4f64360698e2bd9503b1
Alekya Dandu <alekya.dandu@klh.edu.in>
2021-11-13 18:07:50 +0530

    Updated functionality for adding workouts

--- workout tracker/workout.js
+++ workout tracker/workout.js
@@ -3,34 +3,50 @@ const strengthbtn= document.querySelector('.strengthbtn');
 const cardiobtn= document.querySelector('.cardiobtn');
 const sportbtn= document.querySelector('.sportbtn');
 
-let walk=document.querySelector(".walk");
-let strncardio=document.querySelector(".strength-cardio");
-let sport=document.querySelector(".sport")
+const workoutlist=document.querySelector('.workouts');
+let form=document.querySelector(".outerform");
 
-let add= document.querySelector("#addworkout");
+const add= document.querySelector(".addworkout");
+let date =document.querySelector('#date');
+let duration =document.querySelector('#duration');
+let calories =document.querySelector('#calories');
+let workouttype="";
 
-walkbtn.addEventListener('click',addwalk);
-strengthbtn.addEventListener('click',addstrncardio);
-cardiobtn.addEventListener('click',addstrncardio);
-sportbtn.addEventListener('click',addsport);
+walkbtn.addEventListener('click',walk);
+strengthbtn.addEventListener('click',strength);
+cardiobtn.addEventListener('click',cardio);
+sportbtn.addEventListener('click',sport);
 add.addEventListener('click',addworkout);
 
-function addwalk(){
-    walk.style.display="block";
-    strncardio.style.display="none";
-    sport.style.display="none";
-}
-function addstrncardio(){
-    walk.style.display="none";
-    strncardio.style.display="block";
-    sport.style.display="none";
-}
-function addsport(){
-    walk.style.display="none";
-    strncardio.style.display="none";
-    sport.style.display="block";
+function displayform(){
+    form.style.display="block";
 }
 
-function addworkout(e){
+function walk(){
+    displayform();
+    workouttype="Walking/Running";
+}
 
+function strength(){
+    displayform();
+    workouttype="Strength";
+}
+
+function cardio(){
+    displayform();
+    workouttype="Cardio";
+}
+
+function sport(){
+    displayform();
+    workouttype="Sports";
+}
+function addworkout(){
+    const workout=document.createElement('p');
+    workout.innerHTML=`Workout Type-<em>${workouttype}</em><br>Date-<em>${date.value}</em><br>Workout Duration-<em>${duration.value}</em><br>Calories Burned-<em>${calories.value}</em>`;
+    workoutlist.append(workout);
+    form.style.display="none";
+    date.value="";
+    duration.value="";
+    calories.value="";
 }
